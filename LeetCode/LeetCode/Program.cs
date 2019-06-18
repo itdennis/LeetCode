23060db9243ed71895d664c64db3fd5c05d79d78
dennis wu <15040554157@163.com>
2019-06-19 07:50:07 +0800

    update at 2019-6-19 07:49:55

--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -6,7 +6,20 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ListNode node1 = new ListNode(2);
+            ListNode node2 = new ListNode(4);
+            ListNode node3 = new ListNode(3);
+            node1.next = node2;
+            node2.next = node3;
+
+            ListNode nodeA = new ListNode(5);
+            ListNode nodeB= new ListNode(6);
+            ListNode nodeC = new ListNode(4);
+            nodeA.next = nodeB;
+            nodeB.next = nodeC;
+
+            Solution_2_AddTwoNumbers solution_2_AddTwoNumbers = new Solution_2_AddTwoNumbers();
+            solution_2_AddTwoNumbers.AddTwoNumbers(node1, nodeA);
         }
     }
 }
---
+++ LeetCode/LeetCode/Solution_2_AddTwoNumbers.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+    public class Solution_2_AddTwoNumbers
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            //input validation
+            if (l1 == null || l2 == null)
+            {
+                return null;
+            }
+            var l1Result = ListNodeConvert2Num(l1);
+            var l2Result = ListNodeConvert2Num(l2);
+            var output = l1Result + l2Result;
+            return NumConvert2ListNode(output);
+        }
+        private int ListNodeConvert2Num(ListNode node)
+        {
+            int result = 0;
+            Stack nodes = new Stack();
+            while (node != null)
+            {
+                nodes.Push(node.val);
+                node = node.next;
+            }
+            while (nodes.Count > 0)
+            {
+                int a = nodes.Count;
+                result += ((int)nodes.Pop() * (int)Math.Pow(10, a -1));
+            }
+
+            return result;
+        }
+
+        //num = 111;
+        private ListNode NumConvert2ListNode(int num)
+        {
+            Stack nodes = new Stack();
+            while (num > 0)
+            {
+                int a = 1;
+                while (num % (Math.Pow(10, a)) > 10)
+                {
+                    a++;
+                }
+                int currentNum = num / (int)(Math.Pow(10, a));
+                nodes.Push(currentNum);
+                num = num - (currentNum * (int)Math.Pow(10, a));
+            }
+            ListNode node = null;
+            if (nodes.Count > 0)
+            {
+                node = new ListNode((int)nodes.Pop());
+                while (nodes.Count > 0)
+                {
+                    node.val = (int)nodes.Pop();
+                    node = node.next;
+                }
+            }
+            return node;
+        }
+    }
+}
