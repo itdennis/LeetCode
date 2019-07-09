0c3e9fb1ec07441616741ff3beb73cd48a1a16d9
dennis wu <15040554157@163.com>
2019-07-09 08:02:49 +0800

    update at 2019-7-9 08:02:47

--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -6,6 +6,13 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
+            TestCase3();
+        }
+        /// <summary>
+        /// not pass
+        /// </summary>
+        private static void TestCase2()
+        {
             ListNode node1 = new ListNode(2);
             ListNode node2 = new ListNode(4);
             ListNode node3 = new ListNode(3);
@@ -13,13 +20,19 @@ namespace LeetCode
             node2.next = node3;
 
             ListNode nodeA = new ListNode(5);
-            ListNode nodeB= new ListNode(6);
+            ListNode nodeB = new ListNode(6);
             ListNode nodeC = new ListNode(4);
             nodeA.next = nodeB;
             nodeB.next = nodeC;
 
             Solution_2_AddTwoNumbers solution_2_AddTwoNumbers = new Solution_2_AddTwoNumbers();
             solution_2_AddTwoNumbers.AddTwoNumbers(node1, nodeA);
+        }
+
+        private static void TestCase3()
+        {
+            Solution_7_ReverseInt solution_7_ReverseInt = new Solution_7_ReverseInt();
+            solution_7_ReverseInt.Reverse(100);
         }
     }
 }
---
+++ LeetCode/LeetCode/Solution_7_ReverseInt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode
+{
+    public class Solution_7_ReverseInt
+    {
+        public int Reverse(int x)
+        {
+            int result = 0;
+            Stack<int> nodes = new Stack<int>();
+            while (x > 0)
+            {
+                int a = 1;
+                while (x % (Math.Pow(10, a)) > 10)
+                {
+                    a++;
+                }
+                int currentNum = x / (int)(Math.Pow(10, a));
+                nodes.Push(currentNum);
+                x = x - (currentNum * (int)Math.Pow(10, a));
+            }
+            return result;
+        }
+    }
+}
